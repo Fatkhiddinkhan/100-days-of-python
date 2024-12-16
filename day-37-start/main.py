060eb9c2b0af0389b2fcf1caede4cdb129c4fcd3
Fatkhiddin <fatkhiddinkhan@icloud.com>
2024-12-16 15:19:22 +0900

    Function add error handling
    Updated the function to improve retry logic

--- day-37-start/main.py
+++ day-37-start/main.py
@@ -1,6 +1,9 @@
 import requests
 import os
 from datetime import datetime
+import time
+
+
 USERNAME = "fatkhiddin"
 TOKEN = os.environ.get("TOKEN")
 MY_GRAPH = "graph1"
@@ -32,18 +35,31 @@ header = {
 
 # response = requests.post(url=graph_endpoint, json=graph_config, headers=header)
 # print(response.text)
-
+def retry_requesting():
+    retries = 0
+    max_retries = 5
+    while retries < max_retries:
+        response1 = requests.post(url=pixel_endpoint, json=pixel_params, headers=header)
+        if response1.status_code == 200:
+            print("request done")
+            break
+        else:
+            retries += 1
+            print(f"Retry {retries}/{max_retries} in 2 seconds...")
+            time.sleep(2)
+    else:
+        print("Max retries reached. Please try again later.")
 pixel_endpoint = f"{pixela_url}/{USERNAME}/graphs/{MY_GRAPH}"
-while True:
-    today = datetime.now()
-    pixel_params = {
-        "date": today.strftime("%Y%m%d"),
-        "quantity": input("how many hours did you study, today."),
-    }
-    response = requests.post(url=pixel_endpoint, json=pixel_params, headers=header)
-    print(response.text)
-    if response.status_code != 503:
-        break
+
+today = datetime.now()
+pixel_params = {
+    "date": today.strftime("%Y%m%d"),
+    "quantity": input("how many hours did you study, today."),
+}
+response = requests.post(url=pixel_endpoint, json=pixel_params, headers=header)
+if response.status_code == 503:
+    print("requesting again")
+    retry_requesting()
 
 
 put_endpoint = f"{pixela_url}/{USERNAME}/graphs/{MY_GRAPH}/{today.strftime("%Y%m%d")}"
